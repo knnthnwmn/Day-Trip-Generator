ab77102eb90b505a9dd3e1e357fd4dc01596562c
Kenneth Newman <kennethnewman80@gmail.com>
2021-10-07 19:07:09 -0500

    updated the  change in selection

--- Destination.js
+++ Destination.js
@@ -6,7 +6,7 @@ let randomTripArray = [];
 let destinations =["Austin", "Dallas", "Niagra Falls", "New York City", "Houston"];
 let restaurants = ["Outback Steak House", "Pappadeaux Seafood Kitchen", "Benihana Japanese Steakhouse", "Whiskey Cake", "Mignon" ];
 let transportation = ["Lyft", "plane", "train", "boat", "personal transportation"];
-let entertainment = [" go bowling", " play pool", "visit a sports bar", "go to a night club", "visit a theme parks"];
+let entertainment = ["go bowling", "play pool", "visit a sports bar", "go to a night club", "visit a theme park"];
 
 
 function randomTripsGenerator(randomArray) {
@@ -28,17 +28,31 @@ let randomTripGenerator = '';
 
 while (userInput !=6) {
     randomTripGenerator = "You are traveling to" + " " + randomTripArray[0] + ", " + "while there you will be eating at" + " " + randomTripArray[1] + ", " + "your transportation method will be by" + " " + randomTripArray[2] + ", " + 
-    "and your entertainment for the day is to " + " " + randomTripArray[3];
+    "and your entertainment for the day is to" + " " + randomTripArray[3];
     alert(randomTripGenerator);
     
-    let userInput = prompt( "are you happy with results?");
+    userInput = prompt("Select the option you option you would like to change" +
+    
+    "\r\n" + "1 destinations" + "\r\n" + "2 restaurants" + "\r\n" + "3 transportation" + "\r\n" + "4 entertainment" + "\r\n" + "\r\n" + "6 Confirm");
+    
 
 
-
+    if (userInput == 1) {
+        itineraryGenerator(randomTripArray, 0, destinations);
+    } else if (userInput == 2) {
+        itineraryGenerator(randomTripArray, 1, restaurants);
+    } else if (userInput == 3) {
+        itineraryGenerator(randomTripArray, 2, modeOfTransportation);
+    } else if (userInput == 4) {
+        itineraryGenerator(randomTripArray, 3, entertainment); 
+    }
+    else if (userInput = 6) {
+        alert("Your Trip is Complete!" + " " + randomTripGenerator);
+    }
 
 }
 
+console.log(randomTripGenerator);
 console.log(randomTripArray);
-
 
 
